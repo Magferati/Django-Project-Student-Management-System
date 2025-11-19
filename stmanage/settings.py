9e0c5e5a04537d5fbbab2ce7452f70d3705ae050
Candidate <maishaarin4955@gmail.com>
2025-11-19 14:35:44 +0600

    token-time-add

--- stmanage/settings.py
+++ stmanage/settings.py
@@ -31,10 +31,30 @@ AUTH_USER_MODEL = 'authentications.CustomUser'
 from datetime import timedelta #for access token add more time
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=3600),   # এখানে সময় বাড়ান
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
-}
-
+        'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+        'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+        'ROTATE_REFRESH_TOKENS': False,
+        'BLACKLIST_AFTER_ROTATION': False,
+        'UPDATE_LAST_LOGIN': False,
+        'ALGORITHM': 'HS256',
+        'SIGNING_KEY': 'your-secret-key', # Replace with a strong secret key
+        'VERIFYING_KEY': None,
+        'AUDIENCE': None,
+        'ISSUER': None,
+        'JWK_URL': None,
+        'LEEWAY': 0,
+        'AUTH_HEADER_TYPES': ('Bearer',),
+        'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+        'USER_ID_FIELD': 'id',
+        'USER_ID_CLAIM': 'user_id',
+        'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+        'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+        'TOKEN_TYPE_CLAIM': 'token_type',
+        'JTI_CLAIM': 'jti',
+        'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'exp',
+        'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+        'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+    }
 
 # Application definition
 
